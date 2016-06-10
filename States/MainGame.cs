c8c6f44f6b3409b044ca9913046e1db9ee543389
Anton Nikolov <antonio85pd@gmail.com>
2016-06-10 16:23:55 +0300

    background draw

--- States/MainGame.cs
+++ States/MainGame.cs
@@ -4,6 +4,7 @@ using Baligo.Graphics;
 using Baligo.Main;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Baligo.States
 {
@@ -31,10 +32,75 @@ namespace Baligo.States
                 new Vector2(BaligoEngine.Width / 2 - 150, BaligoEngine.Height / 2 - 20),
                 Color.White);
 
+            //Draw background
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 43; j++)
+                {
+                    if (i == 0)
+                    {
+                        Assets.WallBackground.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    else
+                    {
+                        if (j == 0 || j == 42)
+                        {
+                            Assets.WallBackground.Draw(spriteBatch, j * 32, i * 32);
+                        }
+                        else
+                        {
+                            Assets.GrassTypeOneBackground.Draw(spriteBatch, j * 32, i * 32);
+                            /*if (i % 4 == 0 && j % 3 == 0)
+                            {
+                                Assets.GrassTypeTwoBackground.Draw(spriteBatch, j * 32, i * 32);
+                            }
+                            else
+                            {
+                                Assets.GrassTypeOneBackground.Draw(spriteBatch, j * 32, i * 32);
+                            }*/
+                        }
+                    }
+                    //trees
+                    if ((i % 9 == 3) && (j % 13 >= 9))
+                    {
+                        Assets.GrassTypeTwoBackground.Draw(spriteBatch, j * 32, i * 32);
+                        Assets.TreeBackground.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    else if ((i % 14 == 5 || i % 14 == 6) && (j % 17 == 3 || j % 17 == 4))
+                    {
+                        Assets.GrassTypeTwoBackground.Draw(spriteBatch, j * 32, i * 32);
+                        Assets.TreeBackground.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    //lava
+                    if ((i >= 18 && i <= 20) && (j >= 31 && j <= 33))
+                    {
+                        Assets.LavaBackground.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    else if ((i >= 4 && i <= 7) && (j >= 9 && j <= 12))
+                    {
+                        Assets.LavaBackground.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    //doors
+                    if ((i == 1 && j == 0) || (i == 23 && j == 42))
+                    {
+                        Assets.ClosedDoor.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    //fountain
+                    if ((i == 15 || i == 16) && (j == 10 || j == 11))
+                    {
+                        Assets.GrassTypeTwoBackground.Draw(spriteBatch, j * 32, i * 32);
+                        Assets.Fountain.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                    else if ((i >= 6 && i <= 7) && (j >= 30 && j <= 31))
+                    {
+                        Assets.GrassTypeTwoBackground.Draw(spriteBatch, j * 32, i * 32);
+                        Assets.Fountain.Draw(spriteBatch, j * 32, i * 32);
+                    }
+                }
+            }
+
             // Draw current player class
             Player.CurrentPlayerClass.Draw(spriteBatch);
-
-            Assets.PlayerMage.Draw(spriteBatch,0,0);
         }
     }
 }
