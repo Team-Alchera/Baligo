8ed38cb3cc6a0c9ba25a0f7a39e26a80db789fbf
Ivailo Korakov <ivailo98@gmail.com>
2016-06-23 03:04:17 +0300

    Added test app

---
+++ AStar/MainEngine.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AStar
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MainEngine : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public MainEngine()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1152;
+            graphics.PreferredBackBufferHeight = 768;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            World.Init(Content);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            World.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            World.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ AStar/World.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AStar
+{
+    public static class World
+    {
+        public static Point StartPoint;
+        public static Point FinishPoint;
+
+        public static Texture2D Box;
+        private static int _count;
+        public static Point[,] Board;
+
+        public static void Init(ContentManager content)
+        {
+            _count = 7;
+            Board = new Point[12, 18];
+
+            for (int row = 0; row < 12; row++)
+            {
+                for (int col = 0; col < 18; col++)
+                {
+                    Board[row, col] = new Point(row, col);
+                }
+            }
+
+            Box = content.Load<Texture2D>("box");
+        }
+
+
+        public static void Update()
+        {
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed && _count == 0)
+            {
+                UpdateBox();
+                _count = 7;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A) && _count == 0)
+            {
+                SetStartPoint();
+                _count = 7;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S) && _count == 0)
+            {
+                SetFinishPoint();
+                _count = 7;
+            }
+
+            if (_count - 1 >= 0)
+            {
+                _count--;
+            }
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            for (int row = 0; row < 12; row++)
+            {
+                for (int col = 0; col < 18; col++)
+                {
+                    spriteBatch.Draw(Box, new Vector2(col*64, row*64), new Rectangle(0, 0, 64, 64),
+                        Board[row, col].IsSolid ? Color.Black : Color.White);
+                }
+            }
+
+            // Draw Start
+            if (StartPoint != null)
+            {
+                spriteBatch.Draw(Box, new Vector2(StartPoint.Col * 64, StartPoint.Row * 64), new Rectangle(0, 0, 64, 64), Color.LimeGreen);
+            }
+
+            // Draw Finish
+            if (FinishPoint != null)
+            {
+                spriteBatch.Draw(Box, new Vector2(FinishPoint.Col * 64, FinishPoint.Row * 64), new Rectangle(0, 0, 64, 64), Color.Blue);
+            }
+        }
+
+        public static void SetStartPoint()
+        {
+            int col = Mouse.GetState().X / 64;
+            int row = Mouse.GetState().Y / 64;
+
+            if (Board[row,col].IsSolid == false)
+            {
+                if (row >= 0 && row < 12)
+                {
+                    if (col >= 0 && col < 18)
+                    {
+                        StartPoint = new Point(row, col);
+                        Console.WriteLine("New start point");
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Start Point!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static void SetFinishPoint()
+        {
+            int col = Mouse.GetState().X / 64;
+            int row = Mouse.GetState().Y / 64;
+
+            if (Board[row, col].IsSolid == false)
+            {
+                if (row >= 0 && row < 12)
+                {
+                    if (col >= 0 && col < 18)
+                    {
+                        FinishPoint = new Point(row, col);
+                        Console.WriteLine("New Finish Point");
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Finish Point!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static void UpdateBox()
+        {
+            int col = Mouse.GetState().X / 64;
+            int row = Mouse.GetState().Y / 64;
+
+            if (row >= 0 && row < 12)
+            {
+                if (col >= 0 && col < 18)
+                {
+                    Board[row, col].IsSolid = !Board[row, col].IsSolid;
+                    Console.WriteLine("Pressed");
+                }
+            }
+        }
+    }
+}
